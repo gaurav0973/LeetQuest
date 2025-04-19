210e175f2b32b4ba8ac121bdf7e47928dc342b22
gaurav0973 <codejourneypanda@gmail.com>
2025-04-20 00:21:52 +0530

    Folder Setup

---
+++ backend/utils/api-error.js
@@ -0,0 +1,9 @@
+class ApiError{
+    constructor(statusCode, message="Error"){
+        this.statusCode = statusCode,
+        this.message = message,
+        this.success = statusCode >= 400
+    }
+}
+
+export {ApiError}
