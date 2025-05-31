884b2d2ff5e3c5c615d256fbb8be9d9ff00e6427
gaurav0973 <codejourneypanda@gmail.com>
2025-05-31 21:59:09 +0530

    authStore creation & axios

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {Routes, Route, Navigate} from "react-router-dom"
 import HomePage from './page/HomePage'
 import LoginPage from './page/LoginPage'
 import SignUpPage from './page/SignUpPage'
+import { Toaster } from 'react-hot-toast'
+import { useAuthStore } from './store/useAuthStore'
+import { check } from '../../backend/src/controllers/auth.controllers'
 
 function App() {
-  let authUser = null
+    const {authUser, isCheckingAuth, checkAuth} = useAuthStore();
+
+    useEffect(()=>{
+      checkAuth();
+    }, [checkAuth])
+
+
+
   return (
     <div className='flex flex-col justify-start items-center'>
+      <Toaster />
       <Routes>
         <Route 
           path='/' 
           element={authUser ? <HomePage/> : <Navigate to='/login'/>}
-
         />
         <Route 
           path='/login' 
@@ -22,9 +32,7 @@ function App() {
           path='/signup' 
           element={!authUser ? <SignUpPage/> : <Navigate to='/'/>}
         />
-        
       </Routes>
-
     </div>
   )
 }
