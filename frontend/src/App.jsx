74f14517fd7f43417a0499c52344d0d1161ea1ed
gaurav0973 <codejourneypanda@gmail.com>
2025-06-10 15:52:49 +0530

    code iditor with problem statements

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,25 @@
-import React, { useEffect } from 'react'
-import {Routes, Route, Navigate} from "react-router-dom"
-import { Toaster } from 'react-hot-toast'
-import HomePage from './page/HomePage'
-import LoginPage from './page/LoginPage'
-import SignUpPage from './page/SignUpPage'
-import { useAuthStore } from './store/useAuthStore'
-import { Loader } from 'lucide-react'
+import React, { useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
+import HomePage from "./page/HomePage";
+import LoginPage from "./page/LoginPage";
+import SignUpPage from "./page/SignUpPage";
+import { useAuthStore } from "./store/useAuthStore";
+import { Loader } from "lucide-react";
 import Layout from "./layout/Layout";
-import AdminRoute from './components/AdminRoute'
-import AddProblem from './page/AddProblem'
+import AdminRoute from "./components/AdminRoute";
+import AddProblem from "./page/AddProblem";
+import ProblemPage from "./page/ProblemPage";
 
-function App() {
-   const {authUser, checkAuth, isCheckingAuth} = useAuthStore()
+const App = () => {
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
 
-   useEffect(()=>{
-    checkAuth()
-   }, [checkAuth])
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
 
-   if (isCheckingAuth && !authUser) {
+  if (isCheckingAuth && !authUser) {
     return (
       <div className="flex items-center justify-center h-screen">
         <Loader className="size-10 animate-spin" />
@@ -25,26 +27,30 @@ function App() {
     );
   }
 
-
-
   return (
-    <div className='flex flex-col justify-start items-center'>
-      <Toaster/>
+    <div className="flex flex-col items-center justify-start ">
+      <Toaster />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route
             index
-            element={authUser ? <HomePage/> : <Navigate to={"/login"} />}
+            element={authUser ? <HomePage /> : <Navigate to={"/login"} />}
           />
         </Route>
 
-        <Route 
-          path='/login'
-          element={!authUser ? <LoginPage /> : <Navigate to='/' />}
-        />
         <Route
-          path='/signup'
-          element={!authUser ? <SignUpPage /> : <Navigate to='/' />}
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to={"/"} />}
+        />
+
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />}
+        />
+
+        <Route
+          path="/problem/:id"
+          element={authUser ? <ProblemPage /> : <Navigate to={"/login"} />}
         />
 
         <Route element={<AdminRoute />}>
@@ -55,7 +61,7 @@ function App() {
         </Route>
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
